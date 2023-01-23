5076a4a4eb005fd551f64f7709115510686117c5
Brendan McKenna <brendanmckenna4@gmail.com>
2023-01-23 17:20:50 +0000

    added hyperparm

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from continual.python.sdk.runs import Run
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import argparse
 
 def load_config(CONFIG_KEYS):
     config = {}
@@ -113,6 +113,12 @@ def get_metric_id(experiment, key):
             return exp.value
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument('--alpha', type=float, default=os.environ.get('SM_HP_ALPHA'))
+    
+    args, _ = parser.parse_known_args()
+    
     CONFIG_KEYS = [
     "CONTINUAL_APIKEY"
     ]
@@ -162,7 +168,7 @@ if __name__ == "__main__":
 			'eta': 0.1,
 			'num_class': 3,
 			'eval_metric': 'mae',
-			'reg_alpha': 0.5
+			'reg_alpha': args.alpha
 	}
 
     # Splitting the human dataset into the training set and test set
