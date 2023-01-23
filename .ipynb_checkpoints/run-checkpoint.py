c956bd4702eb0e3370556475e6737939bc3adead
Brendan McKenna <brendanmckenna4@gmail.com>
2023-01-23 21:10:48 +0000

    added yaml config file

--- .ipynb_checkpoints/run-checkpoint.py
+++ .ipynb_checkpoints/run-checkpoint.py
@@ -5,18 +5,26 @@ import boto3
 import time
 import pandas as pd
 import os
+import yaml
+
+with open('params.yaml') as config_file:
+    config = yaml.safe_load(config_file)
 
 # Create SageMaker session
 sagemaker_session = sagemaker.Session()
-
-role = "arn:aws:iam::765936207681:role/service-role/AmazonSageMaker-ExecutionRole-20191217T155901"
-
 s3 = boto3.client('s3')
+role = config['environment']['sm_role']
 
+# Model params
+alpha = config['train']['alpha']
+max_dept = config['train']['max_dept']
+eta = config['train']['eta']
+num_class = config['train']['num_class']
+eval_metric = config['train']['eval_metric']
+
+# Continual params
 continual_api_key = os.environ.get("CONTINUAL_API_KEY", None)
 run_id = os.environ.get("CONTINUAL_RUN_ID", None)
-
-alpha = 0.1
 
 # Create estimator
 estimator = SKLearn(
@@ -29,7 +37,7 @@ estimator = SKLearn(
     py_version='py3',
     source_dir='.', 
     environment={"CONTINUAL_APIKEY": continual_api_key, "CONTINUAL_RUN_ID": run_id},
-    hyperparameters={'alpha': alpha}
+    hyperparameters={'alpha': alpha, 'max_dept':max_dept, 'eta':eta, 'num_class':num_class, 'eval_metric':eval_metric}
 )
 
 train_sklearn = "sklearn-training-job-{}".format(int(time.time()))
